0cfad41c01c9f1e2a6daed8a4a84774d15c8ab8d
vladan <vladan@vladanhp.(none)>
2010-09-20 10:36:06 +0200

    Izbrisani fajlovite sto bea staveni po greska

--- KIKA/hacklab/biblioteka/admin.py
+++
@@ -1,6 +0,0 @@
-from hacklab.biblioteka.models import Author, Book
-from django.contrib import admin
-
-
-admin.site.register(Book)
-admin.site.register(Author)
--- KIKA/hacklab/registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
-from django.contrib.auth.views import login, logout, password_change, password_change_done, password_reset, password_reset_done
-
-
-urlpatterns = patterns('hacklab.registration.views',
-	(r'^/$', 'register'),
-	(r'^/success/$', direct_to_template, {'template': 'registration/registration_complete.html'}),
-)
